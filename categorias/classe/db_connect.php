c54c4e0acc2f1d26c59f6f99b2ee72c2cdfd0646
Guilherme Perroni <guilhermeroni@gmail.com>
2020-03-25 12:54:47 -0300

    banco configurado para servidor

--- categorias/classe/db_connect.php
+++ categorias/classe/db_connect.php
@@ -1,14 +1,14 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "vertrigo";
-$db_name = "crud";
-
 //$servername = "localhost";
-//$username = "rootGuilherme";
-//$password = "vertrigoGuilherme";
-//$db_name = "crudGuilherme";
+//$username = "root";
+//$password = "vertrigo";
+//$db_name = "crud";
+
+$servername = "localhost";
+$username = "u476136940_rootGuilherme";
+$password = "guilherme850427";
+$db_name = "u476136940_crudGuilherme";
 
 $connect = mysqli_connect($servername,$username,$password,$db_name);
 mysqli_set_charset($connect, 'utf8');
